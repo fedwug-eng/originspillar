49dcec114cf2322cc25d62c44788d1fab742e1ae
Ahmed <ahmed@originspillar.com>
2026-02-22 01:29:34 +0400

    build: enable webpack debug to trace vercel silent crash

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    // Force verbose server logging 
+    serverSourceMaps: false,
+  },
+  webpack: (config) => {
+    config.infrastructureLogging = { debug: /Packer/ }
+    return config
+  }
 };
 
 export default nextConfig;
